374f15e89e1aad87ce861407a5cd771ed5f3f438
Alban Tiberghien <alban.tiberghien@gmail.com>
2017-11-19 19:24:24 +0100

    Change actuator state as global

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 ACTUATOR_PIN = 8
+GPIO.setup(ACTUATOR_PIN, GPIO.IN)
+ACTUATOR_STATE = GPIO.input(ACTUATOR_PIN)
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,18 +19,21 @@ def index():
     req = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Florac,fr&units=metric&appid=2249f831ffa31996fe0747849b7a8a21')
     data = json.loads(req.text)["main"]
     data["indoor_temp"] = W1ThermSensor().get_temperature()
-    GPIO.setup(ACTUATOR_PIN, GPIO.IN)
-    data["actuator_state"] = GPIO.input(ACTUATOR_PIN)
+    data["actuator_state"] = ACTUATOR_STATE
     return render_template('index.html', data=data)
 
 @app.route("/activate")
 def activate():
+    global ACTUATOR_STATE
+    ACTUATOR_STATE = True
     GPIO.setup(ACTUATOR_PIN, GPIO.OUT)
     GPIO.output(ACTUATOR_PIN, True)
     return "Ventilateur activé"
 
 @app.route("/deactivate")
 def deactivate():
+    global ACTUATOR_STATE
+    ACTUATOR_STATE = False
     GPIO.setup(ACTUATOR_PIN, GPIO.OUT)
     GPIO.output(ACTUATOR_PIN, False)
     return "Ventilateur désactivé"
